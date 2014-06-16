5c07dd029f08019daef460d4359e13954c199bbe
Karl Stoney <karlstoney@googlemail.com>
2014-06-16 20:38:50 +0100

    Initial projectManager creation

---
+++ test/projectManager.test.js
@@ -0,0 +1,13 @@
+'use strict';
+var ProjectManager = require('../lib/projectManager');
+
+describe('projectManager', function() {
+  var projectManager;
+
+  beforeEach(function() {
+    projectManager = new ProjectManager();
+  });
+
+  it('should allow you to create a new project', function() { 
+  });
+});
